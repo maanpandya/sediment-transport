130be5272f634469b8ae1a814da82288d65f3ee5
GamingValter14 <Valter.somlai@icloud.com>
2024-11-17 18:56:18 +0100

    Multiple harmonic basic feature works

--- Duffing Equation/Harmonic Balance/duffing_multiple_harmonics.jl
+++ Duffing Equation/Harmonic Balance/duffing_multiple_harmonics.jl
@@ -1,7 +1,7 @@
 using HarmonicBalance
 
-n_cases = 3 #Defines the number of different driving frequencies to try
-n_harmonics = 1 #Defines the number of harmonics to use
+n_cases = 100 #Defines the number of different driving frequencies to try
+harmonics = [1] #Defines the harmonic to use as an integer which multiplies the fundamental harmonic
 print_harmonic_eqs = true #Set to print or not the harmonic equations
 
 #α -> linear stiffness coefficient
@@ -14,12 +14,11 @@ print_harmonic_eqs = true #Set to print or not the harmonic equations
 
 diff_eq = DifferentialEquation(d(x,t,2) + α*x + β*x^3 + δ*d(x,t) ~ γ*cos(ω*t), x)
 
-fixed = (α => 1.0, β => 1.0, δ => 0.3, γ=>0.3)   #fixed parameters
-varied = ω => range(1.0, 1.5, n_cases)           #range of driving frequencies
+fixed = (α => 1.0, β => 0.04, δ => 0.1, γ=>1)   #fixed parameters
+varied = ω => range(0.01, 3, n_cases)           #range of driving frequencies
 
-harmonics = filter(x -> x % 2 != 0, collect(1:2*n_harmonics)) #Harmonics or multiples of ω
-for i in harmonics
-    add_harmonic!(diff_eq, x, [i*ω]) #add the ith-harmonic
+for i in 1:length(harmonics)
+    add_harmonic!(diff_eq, x, [harmonics[i]*ω]) #add the ith-harmonic
 end
 
 harmonic_eq = get_harmonic_equations(diff_eq)
@@ -29,12 +28,14 @@ if print_harmonic_eqs
 end
 
 result = get_steady_states(harmonic_eq, varied, fixed)
+println(result)
 
 #Branch 1 always contains the real and stable solutions
-#stable_classification = classify_branch(result, "stable")
-#physical_classification = classify_branch(result, "physical")
-#println(stable_classification)
-#println(physical_classification)
+stable_classification = classify_branch(result, "stable")
+physical_classification = classify_branch(result, "physical")
+println("Stable and physical solution are located in the following branches:")
+println(stable_classification)
+println(physical_classification)
 
 #sol = get_single_solution(result, branch=1, index=2)
 #println(sol)
@@ -50,13 +51,25 @@ for j in 1:n_cases
         push!(key_list, key)
     end
 
-    for g in 1:n_harmonics
-        local u = real(sol[key_list[harmonics[g]]])
-        local v = real(sol[key_list[harmonics[g]+1]])
+    for g in 1:2:2*length(harmonics)
+        local u = sol[key_list[g]]
+        local v = sol[key_list[g+1]]
         push!(single_sol_coeffs, u)
         push!(single_sol_coeffs, v)
     end
+    local ω_case = real(sol[key_list[2*length(harmonics)+1]])
+    push!(single_sol_coeffs, ω_case)
     push!(sol_coeffs, single_sol_coeffs)
 end
 
 println(sol_coeffs)
+
+
+
+#plot(result, "sqrt(u1^2 + v1^2)")
+#savefig("C:\\Users\\LENOVO\\Desktop\\duffing_multiple_response.png")
+
+#if plot_harmonic_coefficients
+#    plot(xlabel="ω", ylabel="u" title="Harmonic Coefficients", size=(700, 600), titlepadding=100)
+#
+#end
--- Duffing Equation/Harmonic Balance/test_file.jl
+++ Duffing Equation/Harmonic Balance/test_file.jl
@@ -1,13 +1,4 @@
-function find_drop_index(lst)
-    for i in 1:(length(lst) - 1)  # Iterate until second to last element
-        if lst[i] > lst[i + 1]   # Check if the next element is smaller
-            return i              # Return the index of the drop
-        end
-    end
-    return nothing  # Return nothing if no drop is found
+for g in 1:2:6
+    println(g)
 end
 
-# Example usage
-lst = [1, 3, 5, 4, 6, 8]
-index = find_drop_index(lst)
-println(index)  # Output: 3 (where 5 > 4)
