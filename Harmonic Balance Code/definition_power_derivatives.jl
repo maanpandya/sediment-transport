986965f6a7b874bada9f09821de71cbd2a56b553
GamingValter14 <Valter.somlai@icloud.com>
2024-12-28 15:59:38 +0100

    Deleted a file

--- Harmonic Balance Code/definition_power_derivatives.jl
+++ Harmonic Balance Code/definition_power_derivatives.jl
@@ -38,24 +38,39 @@ function power_derivatives(t, ansatz, powers, derivatives)
 
     #Power calculation
     
-    r1 = @rule cos((~x))^2 => 0.5 + 0.5*cos(2*(~x))
-    r2 = @rule sin((~x))^2 => 0.5 - 0.5*sin(2*(~x))
-    r3 = @rule sin((~x))cos((~x)) => sin(2*(~x))*0.5
-    r4 = @rule cos((~x))sin((~x)) => sin(2*(~x))*0.5
-    r5 = @rule cos((~x))^3 => 0.75*cos((~x)) + 0.25*cos(3*(~x)) 
-    r6 = @rule sin((~x))^3 => 0.75*sin((~x)) - 0.25*sin(3*(~x)) 
-    r7 = @rule (cos((~x))^2)*sin((~x)) => 0.25*sin((~x))+0.25*sin(3*(~x))
-    r8 = @rule sin((~x))*(cos((~x))^2) => 0.25*sin((~x))+0.25*sin(3*(~x))
-    r9 = @rule cos((~x))*(sin((~x))^2) => 0.25*cos((~x)) - 0.25*cos(3*(~x))
-    r10 = @rule (sin((~x))^2)*cos((~x)) => 0.25*cos((~x)) - 0.25*cos(3*(~x))
+    r1 = @acrule cos((~x))^2 => 0.5 + 0.5*cos(2*(~x))
+    r2 = @acrule sin((~x))^2 => 0.5 - 0.5*cos(2*(~x))
+
+    r3 = @acrule sin((~x))cos((~x)) => sin(2*(~x))*0.5
+    r4 = @acrule cos((~x))sin((~x)) => sin(2*(~x))*0.5
+
+    r5 = @acrule sin(~ω * ~t)cos(2* ~ω * ~t) => 0.5*sin(3* ~ω * ~t) - 0.5*sin(~ω * ~t)
+    r6 = @acrule cos(~t * ~ω)cos(2* ~t * ~ω) => 0.5*cos(~t * ~ω) + 0.5*cos(3* ~t * ~ω) 
+
+    r7 = @acrule cos((~x))^3 => 0.75*cos((~x)) + 0.25*cos(3*(~x)) 
+    r8 = @acrule sin((~x))^3 => 0.75*sin((~x)) - 0.25*sin(3*(~x))
+
+    r9 = @acrule sin(~x)cos(~y) => 0.5*(sin(~x + ~y) + sin(~x - ~y))
+    r10 = @acrule sin(~x)sin(~y) => 0.5*(cos(~x - ~y) - cos(~x + ~y))
+    r11 = @acrule cos(~x)cos(~y) => 0.5*(cos(~x - ~y) + cos(~x + ~y))
+
+    r12 = @acrule sin(-1 * (~t * ~ω)) => -1 * sin(~t * ~ω)
+    r13 = @acrule cos(-1 * (~t * ~ω)) => cos(~t * ~ω)
 
     ansatz_powers = []
     for j in 1:length(powers)
         local power1 = simplify(expand(ansatz^(powers[j])))
-        local power2 = simplify(expand(power1), RuleSet([r3, r4]))
-        local power3 = simplify(expand(power2), RuleSet([r5, r6])) 
-        local power4 = simplify(expand(power3), RuleSet([r7, r8]))
-        push!(ansatz_powers, simplify(expand(power4), RuleSet([r9, r10])))
+        #println(power1)
+        local power9 = simplify(expand(power1), RuleSet([r1, r2]))
+        #println(power9)
+        local power2 = simplify(expand(power9), RuleSet([r3, r4]))
+        #println(power2)
+        local power3 = simplify(expand(power2), RuleSet([r7, r8])) 
+        #println(power3)
+        local power4 = simplify(expand(power3), RuleSet([r9, r10, r11]))
+        #println(power4)
+        local power5 = simplify(expand(power4), RuleSet([r12, r13]))
+        push!(ansatz_powers, simplify(expand(power5)))
     end
 
     return ansatz_powers, ansatz_derivatives
@@ -63,7 +78,7 @@ end
 
 #Example usage
 @variables t, ω
-ansatz, c = ansatz_definer(t, ω, [1])
+ansatz, c = ansatz_definer(t, ω, [1, 2])
 println(ansatz)
 
 ansatz_powers, ansatz_derivatives = power_derivatives(t, ansatz, [2, 3], [1, 2])
--- Harmonic Balance Code/test_file.jl
+++ Harmonic Balance Code/test_file.jl
@@ -1,7 +1,7 @@
 using Symbolics
 using SymbolicUtils
 
-@syms x::Real A::Real B::Real C::Real w::Real t::Real x::Real
+@syms x::Real A::Real B::Real C::Real w::Real t::Real y::Real
  
 r1 = @acrule sin(~w * ~t)cos(~w * ~t) => 0.5*sin(2* ~w * ~t)
 r2 = @acrule cos(~w * ~t)^2 => 0.5 + 0.5*cos(2* ~w * ~t)
@@ -9,11 +9,15 @@ r3 = @acrule sin(~w * ~t)^2 => 0.5 - 0.5*cos(2* ~w * ~t)
 r4 = @acrule sin(2* ~w * ~t) => 0 
 r5 = @acrule cos(2* ~w * ~t) => 0
 r6 = @acrule cos(~w * ~t)sin(~w * ~t) => ~a* ~b*0.5*sin(2* ~w * ~t)
-r7 = @acrule cos(~w * ~t)cos(2* ~w * ~t) => 0.5*cos(~w * ~t) + 0.5*cos(3* ~w * ~t) 
+r7 = @acrule cos(~w * ~t)cos(2* ~w * ~t) => 0.5*cos(~w * ~t) + 0.5*cos(3* ~w * ~t)
+r8 = @acrule sin(~x)cos(~y) => 0.5*(sin(~x + ~y) + sin(~x - ~y))
+r12 = @acrule sin((~-)*(~a * ~w * ~t)) => - sin((~a * ~w * ~t))
 
-expr = simplify(expand(A*cos(t*ω)*cos(2*t*ω)), RuleSet([r6, r7]))
-println(expr)
-
+expr20 = simplify(expand(A*sin(2*w*t)cos(13*w*t)), RuleSet([r8, r12]))
+println(expr20)
+expr21 = simplify(expand(expr20), RuleSet([r12]))
+println(expr21)
+"""
 expr1 = simplify(expand((A*cos(w*t)+B*sin(w*t))^2))
 println(expr1)
 expr2 = simplify(expand(expr1), RuleSet([r1]))
@@ -22,4 +26,5 @@ expr3 = simplify(expand(expr2), RuleSet([r2,r3]))
 println(expr3)
 expr4 = simplify(expand(expr3), RuleSet([r4,r5]))
 println(expr4)
+"""
 
